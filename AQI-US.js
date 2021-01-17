d0a97d3db5198a965f686d229e129d44def2ee4a
Andrew Chao <yizhev@gmail.com>
2021-01-18 00:58:56 +0800

    Update AQI-US.js

--- AQI-US.js
+++ AQI-US.js
@@ -6,7 +6,7 @@ const aqicnToken = '6672923e14d6e00dd4e0704f83e2586caab9ff8f'
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
+	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=https://raw.githubusercontent.com/YIZHEV/Rule4Me/master/AQI-US.js
 
 	[MITM]
 	hostname = weather-data.apple.com
