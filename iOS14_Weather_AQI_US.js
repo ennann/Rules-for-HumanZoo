8393d149af61b8ae064ed436392061196e37e49a
Elton Zhao <yizhev@gmail.com>
2021-08-30 11:52:29 +0800

    Update iOS14_Weather_AQI_US.js

--- iOS14_Weather_AQI_US.js
+++ iOS14_Weather_AQI_US.js
@@ -4,13 +4,14 @@
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
 const aqicnToken = '6672923e14d6e00dd4e0704f83e2586caab9ff8f'
 
-// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
-/*
-	[Script]
-	iOS14美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS14_Weather_AQI_US.js
+// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。
 
-	[MITM]
-	hostname = weather-data.apple.com
+/*
+[Script]
+iOS14美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=https://raw.githubusercontent.com/YIZHEV/Rules-for-HumanZoo/master/iOS14_Weather_AQI_US.js
+
+[MITM]
+hostname = weather-data.apple.com
 */
 
 const AirQualityStandard = {
